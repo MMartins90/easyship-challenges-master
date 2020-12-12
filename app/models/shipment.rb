c2505de44502860301ad7e2fd8a7b84540323a32
Marco Martins <marco_martins909@hotmail.co.uk>
2020-12-12 21:25:17 +0000

    Wrote test using RSpec according to brief given in the readme file and created function to group and count items by description

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -6,4 +6,9 @@ class Shipment < ApplicationRecord
   validates :origin_country, presence: true
   validates :tracking_number, presence: true, uniqueness: true
   validates :slug, presence: true
+
+  def group_items(count_list=Hash.new(0))
+    shipment_items.each { |item| count_list[item.description] +=1 }
+    count_list.map { |item,count| {description: item,count: count} }.sort_by { |item| -item[:count] }
+  end
 end
--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -2,4 +2,29 @@ require "rails_helper"
 
 RSpec.describe Shipment, type: :model do
   # write your tests here
+  let(:shipment) {
+      Shipment.new(
+        company: Company.new(name: "New Co"),
+        destination_country: "US",
+        origin_country: "UK",
+        tracking_number: "EA 999 999 999 US",
+        slug: "slug",
+        shipment_items: [
+          ShipmentItem.new(description: "iPad", weight: 3),
+          ShipmentItem.new(description: "iPad", weight: 3),
+          ShipmentItem.new(description: "iPad", weight: 3),
+          ShipmentItem.new(description: "iPhone", weight: 2),
+          ShipmentItem.new(description: "iPhone", weight: 2),
+          ShipmentItem.new(description: "Apple Watch", weight: 1)
+        ]
+      )
+    }
+
+  it 'should return grouped shipment items in order of count' do
+    expect(shipment.group_items).to eq [
+      {description: "iPad", count: 3},
+      {description: "iPhone", count: 2},
+      {description: "Apple Watch", count: 1}
+  ]
+  end
 end
